84ebede0541f1c950c1d442b218eb062d5179f8a
Николаева Татьяна <55144879+nikolaevatatyana@users.noreply.github.com>
2020-10-12 11:44:16 +0700

    Update Receiver.java

--- Receiver.java
+++ Receiver.java
@@ -1,42 +1,116 @@
 package ru.nsu.ccfit.nikolaeva.copydetection;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 
-public class Receiver extends Thread {
-    private MulticastSocket socket;
-    private AddressMap addressMap;
+public class Receiver {
+protected MulticastSocket socket = null;
+protected byte[]buf = new byte[256];
 
-    public Receiver(String groupIP, int port, AddressMap addressMap) throws IOException {
-        // initializing members
+private ArrayList<client> clients = new ArrayList<client>();
 
-        InetAddress group = InetAddress.getByName(groupIP);
+InetAddress group;
 
-        socket = new MulticastSocket(port);
-        socket.joinGroup(new InetSocketAddress(group, port), NetworkInterface.getByInetAddress(group));
+long prevTime;
+long deltaTime;
+long timer;
 
-        this.addressMap = addressMap;
-    }
+public Receiver() throws IOException {
+socket = new MulticastSocket(4446);
+group = InetAddress.getByName("230.0.0.0");
+socket.joinGroup(group);
 
-    @Override
-    public void run() {
-        while(!Thread.currentThread().isInterrupted()) {
-            // while our thread is not interrupted doing some work
+prevTime = System.currentTimeMillis();
+deltaTime = 0;
+timer = 0;
+}
 
-            // creating buffer for message symbols
-            byte[] buffer = new byte[1024];
+public void iteration() throws IOException {
 
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+deltaTime = System.currentTimeMillis() - prevTime;
+prevTime = System.currentTimeMillis();
 
-            try {
-                // receiving packet with message
-                socket.receive(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+for (client c : clients) {
+c.increment_time(deltaTime);
+if(c.getTime_millis() > 10000) c.isOnline = false;
+}
 
-            // binding receive-time with sender's address in addressMap
-            addressMap.add(packet.getSocketAddress(), System.currentTimeMillis());
-        }
-    }
+DatagramPacket packet = new DatagramPacket(buf, buf.length);
+try{
+socket.setSoTimeout(1000);
+socket.receive(packet);
+
+
+String received = new String(packet.getData(), 0, packet.getLength());
+
+boolean client_found = false;
+
+for (client c : clients) {
+if(c.getName().equals(packet.getSocketAddress().toString())) {
+client_found = true;
+c.reset_timer();
+c.isOnline = true;
+break;
+}
+}
+
+if(!client_found) {
+clients.add(new client(packet.getSocketAddress().toString()));
+}
+
+} catch (SocketTimeoutException e){}
+
+//System.out.println(packet.getSocketAddress());
+
+timer -= deltaTime;
+
+if(timer < 0){
+timer = 3000;
+
+System.out.print("\033[H\033[2J");
+System.out.flush();
+
+for (client c: clients) {
+if(c.isOnline) System.out.println(c.name);
+}
+}
+}
+
+public void close() {
+try {
+socket.leaveGroup(group);
+} catch (IOException e) {
+e.printStackTrace();
+} finally {
+socket.close();
+}
+}
+
+private class client {
+private String name;
+private long time_millis = 0;
+
+private boolean isOnline;
+
+public String getName() {return name;}
+public long getTime_millis() {return time_millis;}
+
+public client(String name) {
+this.name = name;
+isOnline = true;
+}
+
+public void increment_time(long delta_time_millis) {
+time_millis += delta_time_millis;
+if(time_millis < 0) time_millis = 0;
+}
+
+public void reset_timer() {
+time_millis = 0;
+}
+}
 }
